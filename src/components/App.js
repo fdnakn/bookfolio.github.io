a81cd5e82bbd6d94bd14926899241e383cf54ccd
akin fidancal <akinfidancal@hotmail.com>
2022-06-08 18:14:04 +0300

    bookfolipapp

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BookList from './Booklist';
+
+class App extends React.Component{
+
+    
+
+    render(){
+        return <div><BookList/></div>
+    }
+}
+
+
+export default App;
---
+++ src/context/BookContext.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default React.createContext(
+    [
+        {
+            title: "Bıdo'nun Yazılım Serüveni",
+            author: "Bidoooooo",
+            pageNum:296,
+            imageURL:"https://kbimages1-a.akamaihd.net/Images/89fd6295-b1e1-4e82-9cb5-20d682c3f723/255/400/False/image.jpg",
+            topic:"Hem müşterinin hem yazılımcının hem de yazılımın hakkını vererek yazılım üretelim."
+
+        },
+        {
+            title: "Jandarma Günlükleri",
+            author: "Ogiiiiiiiiiii",
+            pageNum:296,
+            imageURL:"https://i.idefix.com/cache/500x400-0/originals/0001854972001-1.jpg",
+            topic:"Mültecilerle dolu bir serüven..."
+
+        },
+        {
+            title: "Müzik Notları",
+            author: "Çağınnnnnnn",
+            pageNum:296,
+            imageURL:"https://i.idefix.com/cache/500x400-0/originals/0001761326001-1.jpg",
+            topic:"Batı müziği, Osmanlı-Türk müziği, tasavvuf müziği, halk müziği-türkülerimiz, kadîm müzik düşünceleri ve İslâm müzik düşüncesine dair müzik yazılarını bir araya getiren bu eser bize ısrarla ve samimiyetle şu düşünceyi vermektedir: Varlık âhenktir, müzik ise bir âhenk sanatı."
+
+        },
+        {
+            title: "30 Derste Rusça ",
+            author: "Selimmmmmmmmmmmmm",
+            pageNum:296,
+            imageURL:"https://i.idefix.com/cache/500x400-0/originals/0000000163089-1.jpg",
+            topic:"30 Derste Rusça"
+
+        },
+        {
+            title: "Fenerbahce Sözlüğü",
+            author: "Serhattttttt",
+            pageNum:296,
+            imageURL:"https://i.idefix.com/cache/500x400-0/originals/0001841368001-1.jpg",
+            topic:"Aslında zaten taraftarlık baştan ayağa bir tür aşk fikri üzerine kuruludur. Kimi mahallenin en afili abisinden, kimi dayısından, kimi topla buluştuğu an vurulduğu bir futbolcudan devralır takım aşkını. Ve biliyoruz ki; aşk, sen ne yaparsan yap akıl, fikir, mantık filan tanımaz"
+
+        },
+        {
+            title: "Yeni Azerbaycan Edebiyyatı ",
+            author: "Turkhannn",
+            pageNum:296,
+            imageURL:"https://i.idefix.com/cache/500x400-0/originals/0001900062001-1.jpg",
+            topic:"19. əsr və 20. əsrin əvvəlləri Azərbaycan ictimai fikir tarixində çox zəngin, lakin eyni zamanda mürəkkəb və ziddiyyətli dövrü əhatə edir. Bu dövr Azərbaycan mədəniyyətində ideya-estetik nailiyyətlərinə görə seçilən bir mərhələ olub, Azərbaycan ədəbiyyatının inkişafında da təsiredici rol oynamışdır. "
+
+        }
+    ]
+);
